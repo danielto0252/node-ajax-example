c647f56c19d423f0d0203757ec6be76938b43593
Daniel To <danielto0252@gmail.com>
2012-11-29 17:34:54 -0500

    Experiment with storing locals, passing data around and dynamic url routing

--- app.js
+++ app.js
@@ -11,6 +11,23 @@ var express = require('express')
 
 var app = express();
 
+var fred = { User_ID: 61,
+    FirstName: 'Fred',
+    LastName: 'Flinstone',
+    Email: 'flinstone@stone.org',
+    Password: '$2a$10$27GjOhb8akIUbER6.l72jeWap9jA9R2EKKCytlvuUkwBrU/pEjKjy',
+    Gender: null,
+    Birthdate: 'Invalid Date' };
+
+var george = { User_ID: 64,
+    FirstName: 'George',
+    LastName: 'Of The Jungle',
+    Email: 'georgie@jungle.com',
+    Password: '$2a$10$27GjOhb8akIUbER6.l72jeWap9jA9R2EKKCytlvuUkwBrU/pEjKjy',
+    Gender: null,
+    Birthdate: 'Invalid Date' };
+
+
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
@@ -32,6 +49,29 @@ app.get('/users', user.list);
 app.get('/ajax', function(request, response) {
 	response.send({ text: 'Awesome!'});
 });
+
+
+app.get('/testing', function(request, response) {
+  response.render('testing.jade', { title: 'Herro!', locals: { user: fred } });
+});
+
+app.post('/testing', function(request, response) {
+  if(request.body.secretPassword === 'open seasame!') {
+    response.redirect('/user/'+request.body.secretCode);
+  }
+});
+
+app.get('/user/:id', function(request, response) {
+  console.log('rawrrr!!!');
+  console.log(request.params.id);
+  if(request.params.id == '61')
+    response.render('user.jade', { title: 'Greetings!', locals: { user: fred } });
+  if(request.params.id == '64')
+    response.render('user.jade', { title: 'Salutations!', locals: { user: george } });
+  else 
+    redirect('/');
+});
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
